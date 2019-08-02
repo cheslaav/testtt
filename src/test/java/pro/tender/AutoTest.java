116e002100c4d5d3e93383fda3ad1ec6efebe5ab
Vitaliy Nastich <nastich@tender.pro>
2019-08-02 14:11:35 +0300

    Remote WebDriver

--- src/test/java/pro/tender/AutoTest.java
+++ src/test/java/pro/tender/AutoTest.java
@@ -6,7 +6,13 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -18,11 +24,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutoTest {
     @BeforeClass
-    static public void setupTimeout()
-    {
+    static public void setupTimeout() throws MalformedURLException {
         Configuration.timeout = 10000;
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://188.225.18.7:4444/wd/hub"), capability);
 
-        Configuration.browser = "chrome";
+
     }
 }
